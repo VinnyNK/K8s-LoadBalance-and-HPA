d3b9bca5695c5d36ef7198f5e058fef094275480
Vinicius Kerschner <kerschner.vinicius@gmail.com>
2021-12-07 18:04:59 -0300

    Initial commit

---
+++ src/PDI.K8s.LoadBalancer_HPA.MinimalApi/Program.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Controllers
+app.MapGet("/api/localip", () =>
+{
+    var localIp = GetLocalIpAddress();
+    
+    return localIp;
+
+}).WithName("GetInternalIP");
+
+app.MapGet("/health", () => "healthy").WithName("HealthCheck");
+
+app.Run();
+
+static string GetLocalIpAddress()
+{
+    var host = Dns.GetHostEntry(Dns.GetHostName());
+    foreach (var ip in host.AddressList)
+    {
+        if (ip.AddressFamily == AddressFamily.InterNetwork)
+        {
+            return ip.ToString();
+        }
+    }
+    throw new Exception("No network adapters with an IPv4 address in the system!");
+}
